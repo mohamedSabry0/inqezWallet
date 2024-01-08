f0a60d3c81c4a146fcce1f4b0514d0fad339211a
mohamedSabry0 <mohamedsabry3023@gmail.com>
2024-01-08 22:24:14 +0200

    Configure devise

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+
+  validates :name, presence: true, length: { maximum: 250, minimum: 4 }
+  validate :password_complexity
+
+  def password_complexity
+    return if password.blank?
+
+    complexity_errors = []
+
+    complexity_errors << 'one uppercase letter' unless password =~ /[A-Z]/
+    complexity_errors << 'one lowercase letter' unless password =~ /[a-z]/
+    complexity_errors << 'one digit' unless password =~ /[0-9]/
+    complexity_errors << 'one special character' unless password =~ /[#?!@$%^&*-]/
+
+    return if complexity_errors.empty?
+
+    errors.add(:password,
+               "Complexity requirement not met. The password needs at least: #{complexity_errors.join(', and ')}")
+  end
+
+  # override devise confiramable for now
+  def confirmation_required?
+    false
+  end
+end
