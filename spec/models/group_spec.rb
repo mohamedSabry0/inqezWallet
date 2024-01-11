fea0d5a5bdf2b44d5a842fc76ba7760f3e0b8fac
mohamedSabry0 <mohamedsabry3023@gmail.com>
2024-01-11 16:44:30 +0200

    Create unit tests for group

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:payment_groups).dependent(:destroy) }
+    it { should have_many(:payments).through(:payment_groups) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(4).is_at_most(250) }
+    it { should validate_presence_of(:icon) }
+  end
 end
