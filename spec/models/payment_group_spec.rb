1d2f203af7ae4d68b6fd5a1c5d7b124def068f04
mohamedSabry0 <mohamedsabry3023@gmail.com>
2024-01-11 16:45:22 +0200

    Create unit tests for Payment, and PaymentGroup

--- spec/models/payment_group_spec.rb
+++ spec/models/payment_group_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe PaymentGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:group) }
+    it { should belong_to(:payment) }
+  end
 end
